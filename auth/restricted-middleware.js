cc9f5203effc841bf60e00d88736f4a82add696e
Landry Irakoze <irakozel@gmail.com>
2019-09-17 15:14:34 -0400

    updated restriction middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,29 +1,36 @@
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
-const Users = require('../users/users-model');
+// const Users = require('../users/users-model');
 
+// module.exports = (req, res, next) => {
+//     let { username, password } = req.headers;
+
+//     Users.find({ username })
+//         .first()
+//         .then(user => {
+//             if (user && bcrypt.compareSync(password, user.password)) {
+//                 next()
+//             } else {
+//                 res.status(400).json({ message: 'restricted route' })
+//             }
+//         })
+//         .catch(err => {
+//             res.status(500).json(err)
+//         })
+
+//     function fetch() {
+//         const reqOptions = {
+//             headers: {
+//                 username: '',
+//                 password: ''
+//             }
+//         }
+//     }
+// }
 module.exports = (req, res, next) => {
-    let { username, password } = req.headers;
-
-    Users.find({ username })
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                next()
-            } else {
-                res.status(400).json({ message: 'restricted route' })
-            }
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
-
-    function fetch() {
-        const reqOptions = {
-            headers: {
-                username: '',
-                password: ''
-            }
-        }
+    if (req.session && req.session.user) {
+        next();
+    } else {
+        res.status(401).json({ message: 'You shall not pass!' })
     }
 }
